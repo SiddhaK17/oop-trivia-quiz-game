64165915c965d2956653fc6b684b6ca42c9fc19e
Siddha Kadam <siddhakad17@gmail.com>
2026-06-10 03:43:02 +0530

    Refactor question bank initialization and print statements

--- main.py
+++ main.py
@@ -2,17 +2,26 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 
-question_bank = []
-for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
-    new_question = Question(question_text, question_answer)
-    question_bank.append(new_question)
+# question_bank = []
+# for question in question_data:
+#     question_text = question["text"]
+#     question_answer = question["answer"]
+#     new_question = Question(question_text, question_answer)
+#     question_bank.append(new_question)
+
+#                 OR
+
+question_bank = [
+    Question(question["text"], question["answer"])
+    for question in question_data
+]
 
 quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
     quiz.next_question()
 
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+print("\n You've completed the quiz!")
+print(
+    f"Final Score: {quiz.score}/{quiz.question_number}"
+)
